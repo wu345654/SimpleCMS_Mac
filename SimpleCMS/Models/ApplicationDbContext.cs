45a73b49688c0a58a55120629d12e53db15a5b11
wu345654 <wu345654@live.cn>
2020-05-22 17:55:54 +0800

    配置Dbcontext

---
+++ SimpleCMS/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNet.Identity.EntityFramework;
+using NLog;
+
+namespace SimpleCMS.Models
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser,ApplicationRole,int,ApplicationUserLogin,ApplicationUserRole,ApplicationUserClaim>
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public ApplicationDbContext():base("DefaultConnection")
+        {
+            Database.Log = Logger.Info;
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ SimpleCMS/Models/IndentityModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SimpleCMS.Models
+{
+    public class ApplicationUserLogin : IdentityUserLogin<int> { }
+    public class ApplicationUserClaim : IdentityUserClaim<int> { }
+    public class ApplicationUserRole : IdentityUserRole<int> { }
+    public class ApplicationRole : IdentityRole<int, ApplicationRole>, IRole<int>
+    {
+        public string Description { get; set; }
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string name)
+            : this()
+        {
+            this.Name = name;
+        }
+        public ApplicationRole(string name, string description)
+            : this(name)
+        {
+            this.Description = description;
+        }
+    }
+}
